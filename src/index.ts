c8d14c67347008dabe75d735decfa000e3e856ac
Alex Iglesias <alexiglesias@finsweet.com>
2023-10-26 08:35:59 +0200

    fix: linting

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,6 @@ export * from './get';
 export * from './init';
 export * from './query';
 export * from './remove';
+export * from './set';
 export type { DB } from './types';
 export * from './update';
-export * from './set';
